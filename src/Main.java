6eca9355d310e798cc1094e241aa3be01a446207
«duKe» <dyuk0vser@yandex.ru>
2026-07-07 01:09:21 +0300

    Добавить Main

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        ReaderTxt readerTxt = new ReaderTxt();
+        readerTxt.readFile("discount_day_without_ext");
+    }
+}
