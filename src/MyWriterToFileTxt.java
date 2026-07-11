afd1af4e904986cbfe50eb275598dab9600f270b
«duKe» <dyuk0vser@yandex.ru>
2026-07-11 15:50:34 +0300

    Добавь Adapter.

---
+++ src/MyWriterToFileTxt.java
@@ -0,0 +1,19 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
+
+public class MyWriterToFileTxt {
+
+    public void write(Map<String, Double> map, String filePath) throws IOException {
+        Path path = Paths.get(filePath);
+        try (BufferedWriter writer = Files.newBufferedWriter(path)) {
+            for (Map.Entry<String, Double> entry : map.entrySet()) {
+                writer.write(entry.getKey() + "---" + entry.getValue());
+                writer.newLine();
+            }
+        }
+    }
+}
